a2367d6531f9d88665fd38c18315f95fdc049594
zhaitianye <1048972989@qq.com>
2019-10-11 09:15:24 +0800

    修改

--- 常用的ts代码段.ts
+++ 常用的ts代码段.ts
@@ -11,3 +11,6 @@ if (!temp1[item.deliveryAddress.uid]) {
 })
 let temp1_1 = Object.keys(temp1)
 let temp1_2 = Object.values(temp1)
+
+// 生成一个随机日期的随机数
+let aaa = `${Math.floor(Math.random()*30+1)}号,${Math.floor(Math.random()*16+8)}点${Math.floor(Math.random()*60+1)}分${Math.floor(Math.random()*60+1)}秒`;
